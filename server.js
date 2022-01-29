13a21f9671303635620289e8b816c369ec1ead18
Galen Hammond <Galen@Galen-Hammonds-MacBook-Pro.local>
2022-01-29 18:56:11 -0500

    cor

--- server.js
+++ server.js
@@ -5,6 +5,27 @@ const express = require("express");
 const fs = require("fs");
 const cors = require("cors");
 const app = express();
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+
+  // Add this
+  if (req.method === "OPTIONS") {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "PUT, POST, PATCH, DELETE, OPTIONS"
+    );
+    res.header("Access-Control-Max-Age", 120);
+    return res.status(200).json({});
+  }
+
+  next();
+});
+
 app.use(cors());
 const { createServer } = require("https");
 const { Server } = require("socket.io");
